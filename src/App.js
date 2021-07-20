a8c6ea28cd618494cc5e6e196e4ba20c68a628d6
Sabin Sheja <sabin.sheja@oneacrefund.org>
2021-07-20 21:51:02 +0200

    test ci/cd from PR" ;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Edit the code <code>src/App.js</code> and save to reload.
+          Hello, change from branch Edit the code <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
